578861b86c625a7e7b82602c062e6d8a0d8f5f7c
Mac-Coleman <williamcormaccoleman@gmail.com>
2024-04-01 23:53:17 -0500

    Added python setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="telescope",
+    version="0.0.1",
+    description="GoTo Altazimuth Telescope Engineering Capstone Project",
+    url="https://github.com/Mac-Coleman/EGR-385-Telescope-Mount",
+    author="Mac Coleman",
+    author_email="williamcormaccoleman@gmail.com",
+    license="none",
+    packages=["telescope"],
+    install_requires=["RPi.GPIO", "adafruit-circuitpython-gps", "smbus2", "skyfield", "rpi-hardware-pwm"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3"
+    ]
+)
