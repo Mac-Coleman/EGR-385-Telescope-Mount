e55e1fefc11552835ec622aa06328050496b509c
Mac-Coleman <williamcormaccoleman@gmail.com>
2024-04-06 23:52:28 -0500

    Update install script maybe?

--- install.sh
+++ install.sh
@@ -7,4 +7,10 @@ git config pull.rebase false
 sudo -H python3 -m pip install -e .
 sudo cp -v ./bin/telescope.service /lib/systemd/system/
 sudo systemctl enable telescope.service
+cd telescope
+python3 setup_database.py
+echo "enable_uart=1" | sudo tee -a /boot/config.txt
+echo "dtoverlay=pwm-2chan" | sudo tee -a /boot/config.txt
+echo "dtparam=i2c_arm_baudrate=400000" | sudo tee -a /boot/config.txt
+echo "dtoverlay=i2c-gpio,bus=4,i2c_gpio_sda=17,i2c_gpio_scl=27" | sudo tee -a /boot/config.txt
 sudo reboot now
